30995bb7a6a231a1c8fd49944546ed4a5e9d0156
Rahul Khandait <90828823+rahul84206@users.noreply.github.com>
2023-07-12 22:16:00 +0530

    file added

---
+++ JS/Assignment-Questions/Golden-String.js
@@ -0,0 +1,7 @@
+let str = "apple";
+if ((str[0]==="a" || str[0]==="A") && (str.length>5)){
+    console.log("It is a Golden String");
+}
+else{
+    console.log("It is not a Golden String");
+}
---
+++ JS/Assignment-Questions/Name-Age.js
@@ -0,0 +1,3 @@
+let yourName = prompt("Please enter your name");
+let age = prompt("Please enter your age");
+alert(`${yourName} is ${age} years old.`);
