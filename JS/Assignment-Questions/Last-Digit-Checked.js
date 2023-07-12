22ce23928c95240dd138ab9ac97839e390e2e4af
Rahul Khandait <90828823+rahul84206@users.noreply.github.com>
2023-07-12 22:35:21 +0530

    file added

---
+++ JS/Assignment-Questions/Last-Digit-Checked.js
@@ -0,0 +1,10 @@
+let num1 = 32;
+let num2 = 47852;
+
+if ((num1%10) == (num2%10)) {
+    console.log("Numbers have the same last digit which is", num1%10);
+} 
+else {
+    console.log("Numbers don't have the same last digit");
+}
+
