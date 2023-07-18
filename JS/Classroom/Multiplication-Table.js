c208a15f8200ba882e4d3623c1dbbb2b891de18f
Rahul Khandait <90828823+rahul84206@users.noreply.github.com>
2023-07-18 18:48:58 +0530

    file added

--- JS/Classroom/Multiplication-Table.js
+++ JS/Classroom/Multiplication-Table.js
@@ -1,3 +1,11 @@
 for (let i = 5; i <=50; i+=5) {
     console.log(i);
 }
+
+console.log("------------");
+
+let n=prompt("enter a number");
+n=parseInt(n);
+for (let i = n; i <= n*10; i+=n) {
+    console.log(i);
+}
