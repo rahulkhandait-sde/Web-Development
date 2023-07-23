e7135bbbf05f4affaad2b52821e921ac5b694b40
Rahul Khandait <90828823+rahul84206@users.noreply.github.com>
2023-07-23 12:36:33 +0530

    file updated

--- JS/Assignment-Questions/Dice-Roll.js
+++ JS/Assignment-Questions/Dice-Roll.js
@@ -1,2 +1,2 @@
-const randomNumber = Math.ceil(Math.random() *6 );
-console.log(`Random number is ${randomNumber}`);
+const dice = Math.ceil(Math.random() *6 );
+console.log(`Random number is ${dice}`);
