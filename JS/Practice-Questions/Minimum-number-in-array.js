b3568649498cc2681efdd0e18e2b01b84a448e17
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2023-07-29 19:26:10 +0530

    file updated

--- JS/Practice-Questions/Minimum-number-in-array.js
+++ JS/Practice-Questions/Minimum-number-in-array.js
@@ -2,14 +2,25 @@
 
 let num = [10,20,30,40,50,-60,70,80,90,100];
 
-function minNum(arr) {
-    let min = arr.reduce((min, el) => {
-        if(el < min) {
-            min = el;
-        }
-        return min;
-    });
-    return min;
-}
+// function minNum(arr) {
+//     let min = arr.reduce((min, el) => {
+//         if(el < min) {
+//             min = el;
+//         }
+//         return min;
+//     });
+//     return min;
+// }
 
-console.log(minNum(num)); // -60
+// console.log(minNum(num)); // -60
+
+let min = num.reduce((min, el) => {
+    console.log(min, el);   
+    if (min<el) {
+        return min;
+    } else {
+        return el;
+    }
+});
+
+console.log(min); // -60
