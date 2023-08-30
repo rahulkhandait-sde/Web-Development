d20c9191a3978f4e2efbb46670e1a4fb9de5fa25
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2023-08-30 12:12:04 +0530

    initial commit

--- REST-APIs/index.js
+++ REST-APIs/index.js
@@ -10,9 +10,23 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set(express.static(path.join(__dirname, 'public')));
 
+let posts = [
+	{
+		username: 'John',
+		content: 'I love coding',
+	},
+	{
+		username: 'Rahul',
+		content: 'Hard work is the key to success',
+	},
+	{
+		username: 'Sachin',
+		content: 'My favourite sport is cricket',
+	},
+];
 
-app.get ('/', (req, res) => {
-    res.send("Serving working well!");
+app.get('/posts', (req, res) => {
+	res.send('Serving working well!');
 });
 
 app.listen(port, () => {
