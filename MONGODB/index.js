c5c5fa7f7055cb98ee5a47aac8b03950ff8bacf8
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2023-09-23 18:41:07 +0530

    new changes added

--- MONGODB/index.js
+++ MONGODB/index.js
@@ -1,2 +1,49 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+main()
+	.then(() => {
+		console.log('MongoDB Connected...');
+	})
+
+	.catch((err) => console.log(err));
+
+async function main() {
+	await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+	name: String,
+	email: String,
+	age: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+
+const user1 = new User({
+	name: 'John',
+	email: 'john@gmail.com',
+	age: 23,
+});
+const user2 = new User({
+	name: 'Smith',
+	email: 'smith@gmail.com',
+	age: 25,
+});
+
+user1
+	.save()
+	.then((res) => {
+		console.log(res);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+user2
+	.save()
+	.then((res) => {
+		console.log(res);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
