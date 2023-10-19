026b9cfe43652609ddc94f870af7c9b7f2e2938c
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2023-10-19 17:46:45 +0530

    Delete Route Added

--- Express-MongoDB/index.js
+++ Express-MongoDB/index.js
@@ -3,11 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./models/chat.js');
+const methodOverride = require('method-override');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 
 main()
 	.then(() => console.log('MongoDB Connected'))
@@ -48,8 +52,32 @@ app.post('/chats', (req, res) => {
 	res.redirect('/chats');
 });
 
+//Edit Route
+app.get('/chats/:id/edit', async (req, res) => {
+	let { id } = req.params;
+	let chat = await Chat.findById(id);
+	res.render("edit.ejs", {chat})
+});
+
+//Update Route
+app.put('/chats/:id', async (req, res) => {
+	let { id } = req.params;
+	let { message: newMsg } = req.body; 
+	let updateChat = await Chat.findByIdAndUpdate(id, {message: newMsg}, {runValidators: true, new: true});
+	res.redirect('/chats');
+});
+
+//Delete Route
+app.delete('/chats/:id', async (req, res) => {
+	let { id } = req.params;
+	await Chat.findByIdAndDelete(id);
+	res.redirect('/chats');
+});
+
+//Home Route
 app.get('/', (req, res) => {
 	res.send('Root is working');
 });
 
 app.listen(8080, () => console.log('Server running on port 8080'));
+
